7337954073a1ca1a2ec0b04a023915def3f836de
Alexander Mukhin <alexander.i.mukhin@gmail.com>
2020-08-21 13:24:51 +0300

    ascope.ino: removed spaces around assignment operators

--- ascope.ino
+++ ascope.ino
@@ -12,17 +12,17 @@ volatile unsigned char ch; // current channel
 volatile unsigned char rdy; // ready flag
 
 // set and clear bit macros
-#define sbi(port,bit) (port) |= (1<<(bit))
-#define cbi(port,bit) (port) &= ~(1<<(bit))
+#define sbi(port,bit) (port)|=(1<<(bit))
+#define cbi(port,bit) (port)&=~(1<<(bit))
 
 // AC ISR
 ISR(ANALOG_COMP_vect) {
 	// start timer
-	TCCR1B |= cs.prescale;
+	TCCR1B|=cs.prescale;
 	// disable AC interrupts
 	cbi(ACSR,ACIE);
 	// turn on acquisition LED
-	PORTB |= B00100000;
+	PORTB|=B00100000;
 }
 
 // ADC ISR
@@ -32,13 +32,13 @@ ISR(ANALOG_COMP_vect) {
 // will not delay it
 ISR(ADC_vect,ISR_NOBLOCK) {
 	// stop timer
-	TCCR1B &= B11111000;
+	TCCR1B&=B11111000;
 	// reset counter
-	TCNT1 = 0;
+	TCNT1=0;
 	// clear TC1 output compare B match flag
 	sbi(TIFR1,OCF1B);
 	// save conversion result 
-	buf[ch][n] = ADCH;
+	buf[ch][n]=ADCH;
 	// advance position
 	++n;
 	// increase delay
@@ -46,27 +46,27 @@ ISR(ADC_vect,ISR_NOBLOCK) {
 	// is buffer filled?
 	if (n==N) {
 		// reset position and delay
-		n = 0;
-		OCR1B = 1;
+		n=0;
+		OCR1B=1;
 		// consider the next channel
 		++ch;
 		// any channels left?
 		if (ch<cs.chs) {
 			// select the next channel
-			ADMUX = (ADMUX&B11110000)+(ch&B00001111);
+			ADMUX=(ADMUX&B11110000)+(ch&B00001111);
 		} else {
 			// done with the last channel
 			// turn off acquisition LED
-			PORTB &= B11011111;
+			PORTB&=B11011111;
 			// raise ready flag
-			rdy = 1;
+			rdy=1;
 			// we're done
 			return;
 		}
 	}
 	// enable AC interrupts and clear AC interrupt flag as well
 	// (we don't want to process a stale pending interrupt)
-	ACSR |= B00011000;
+	ACSR|=B00011000;
 }
 
 void
@@ -78,9 +78,9 @@ setup () {
 	// left-adjust output
 	sbi(ADMUX,ADLAR);
 	// set ADC clock prescale value
-	ADCSRA = (ADCSRA&B11111000)+2;
+	ADCSRA=(ADCSRA&B11111000)+2;
 	// disable digital input buffer on all ADC pins (ADC0-ADC7)
-	DIDR0 = B11111111;
+	DIDR0=B11111111;
 	// set auto-trigger on Timer/Counter1 Compare Match B
 	sbi(ADCSRB,ADTS2);
 	sbi(ADCSRB,ADTS0);
@@ -98,12 +98,12 @@ setup () {
 	sbi(DIDR1,AIN0D);
 	// stop Timer/Counter0, since we're not using it,
 	// and don't want its ISR to delay our AC ISR
-	TCCR0B &= B11111000;
+	TCCR0B&=B11111000;
 	// init Timer/Counter1
 	// reset control registers to the default values
-	TCCR1A = 0;
-	TCCR1B = 0;
-	TCCR1C = 0;
+	TCCR1A=0;
+	TCCR1B=0;
+	TCCR1C=0;
 	// init serial
 	Serial.begin(9600);
 	// init LED
@@ -113,21 +113,21 @@ setup () {
 #if 0
 // enable calibration PWM output
 // clear TC2 control registers
-TCCR2A = 0;
-TCCR2B = 0;
-TIMSK2 = 0;
+TCCR2A=0;
+TCCR2B=0;
+TIMSK2=0;
 // set output compare register
-OCR2A = 255; // 31.25 kHz
-//OCR2A = 127; // 62.5 kHz
-//OCR2A = 63; // 125 kHz
-//OCR2A = 50;
-//OCR2A = 41;
-//OCR2A = 35;
-//OCR2A = 31; // 250 kHz
-//OCR2A = 26;
-//OCR2A = 23; // 333 kHz
-//OCR2A = 19;
-//OCR2A = 15; // 500 kHz
+OCR2A=255; // 31.25 kHz
+//OCR2A=127; // 62.5 kHz
+//OCR2A=63; // 125 kHz
+//OCR2A=50;
+//OCR2A=41;
+//OCR2A=35;
+//OCR2A=31; // 250 kHz
+//OCR2A=26;
+//OCR2A=23; // 333 kHz
+//OCR2A=19;
+//OCR2A=15; // 500 kHz
 // toggle OC2A on compare
 sbi(TCCR2A,COM2A0);
 // enable CTC (clear counter on compare) mode
@@ -145,13 +145,13 @@ void
 loop () {
 	unsigned char c;
 	// clear ready flag
-	rdy = 0;
+	rdy=0;
 	// select channel 0
-	ch = 0;
-	ADMUX &= B11110000;
+	ch=0;
+	ADMUX&=B11110000;
 	// set initial position and delay
-	n = 0;
-	OCR1B = 1;
+	n=0;
+	OCR1B=1;
 	// set trigger slope
 	if (cs.slope)
 		// trigger on rising edge
@@ -168,18 +168,18 @@ loop () {
 	Serial.write(makecw(cs)); // current control word
 	for (ch=0; ch<cs.chs; ++ch)
 		for (n=0; n<N; ++n) {
-			c = buf[ch][n];
+			c=buf[ch][n];
 			// we write 1 instead of 0
 			// to avoid confusion with the sync marker
 			if (c==0)
-				c = 1;
+				c=1;
 	    		Serial.write(c);
 		}
 	// wait for the transmission to complete
 	Serial.flush();
 	// read the new control word, if available
 	if (Serial.available()) {
-		c = Serial.read();
+		c=Serial.read();
 		// parse control word
 		parsecw(c,&cs);
 	}
