fb090db86db6c44c96123a1a4db15f2688fafbc6
Alexander Mukhin <alexander.i.mukhin@gmail.com>
2020-08-25 13:32:48 +0300

    ascope.h: store chs-1 in CW to save bits

--- ascope.h
+++ ascope.h
@@ -9,7 +9,7 @@
 struct ctl {
 	unsigned char samp; // sampling mode (0=RT,1=ET)
 	unsigned char trig; // trigger mode (1=normal,0=auto) [RT only]
-	unsigned char chs; // current number of channels
+	unsigned char chs; // current number of channels (1..MAXCHS)
 	unsigned char slope; // trigger edge (1=rising,0=falling)
 	unsigned char prescale; // timer clock (ET) or ADC clock (RT) prescale
 };
@@ -17,7 +17,7 @@ struct ctl {
 // make oscilloscope control word
 unsigned char
 makecw (struct ctl c) {
-	return ((c.samp&1)<<7)+((c.trig&1)<<6)+((c.chs&3)<<4)+((c.slope&1)<<3)+(c.prescale&7);
+	return ((c.samp&1)<<7)+((c.trig&1)<<6)+(((c.chs-1)&3)<<4)+((c.slope&1)<<3)+(c.prescale&7);
 }
 
 // parse oscilloscope control word
@@ -25,7 +25,7 @@ void
 parsecw (unsigned char cw, struct ctl *c) {
 	c->prescale=cw&7;
 	c->slope=cw>>3&1;
-	c->chs=cw>>4&3;
+	c->chs=(cw>>4&3)+1;
 	c->trig=cw>>6&1;
 	c->samp=cw>>7&1;
 }
